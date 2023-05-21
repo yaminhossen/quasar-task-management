385b1974434fa209cda434d4572b4783547016e1
yaminhossen <streatdot616@gmail.com>
2023-05-21 16:30:26 +0600

    second

---
+++ src/components/exampleCom.vue
@@ -0,0 +1,13 @@
+<template>
+  <h3>This is new vie component</h3>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -5,16 +5,17 @@
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
     >
-    <banner></banner>
+  <Banner></Banner>
+  <!-- <exampleComVue></exampleComVue> -->
   </q-page>
 </template>
 
 <script>
+import exampleComVue from 'src/components/exampleCom.vue'
 import { defineComponent } from 'vue'
-import Banner from '../pages/Banner.vue';
-
+import Banner from "./Banner.vue"
 export default defineComponent({
-  components: { Banner },
+  components: {Banner},
   name: 'IndexPage'
 })
 </script>
